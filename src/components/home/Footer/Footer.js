6343ec5b9dcdd100758efe5237b77d2842bbba49
ynrrishabh <104955791+ynrrishabh@users.noreply.github.com>
2025-08-04 20:42:35 +0530

    Update Footer.js

--- src/components/home/Footer/Footer.js
+++ src/components/home/Footer/Footer.js
@@ -48,7 +48,7 @@ const Footer = () => {
                 </li>
               </a>
               <a
-                href="https://github.com/noorjsdivs"
+                href="#"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -57,7 +57,7 @@ const Footer = () => {
                 </li>
               </a>
               <a
-                href="https://www.facebook.com/Noorlalu143/"
+                href="#"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -66,7 +66,7 @@ const Footer = () => {
                 </li>
               </a>
               <a
-                href="https://www.linkedin.com/in/noor-mohammad-ab2245193/"
+                href="#"
                 target="_blank"
                 rel="noreferrer"
               >
